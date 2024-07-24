fbbd2b6f4dd5ae21f4abfce5c8fea040d207bb0c
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-07-24 12:10:00 +0300

    🚀Project added

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -12,6 +12,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { getFirebaseApp } from "../utils/firebaseHelper";
 import { child, getDatabase, off, onValue, ref } from "firebase/database";
 import { setChatsData } from "../store/chatSlice";
+import { ActivityIndicator, View } from "react-native";
+import colors from "../constants/colors";
+import commonStyles from "../constants/commonStyles";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -86,6 +89,8 @@ const StackNavigator = () => {
 const MainNavigator = (props) => {
     const dispatch = useDispatch();
 
+    const [isLoading, setIsLoading] = useState(true);
+
     const userData = useSelector(state => state.auth.userData);
     const storedUsers = useSelector(state => state.users.storedUsers);
 
@@ -121,8 +126,13 @@ const MainNavigator = (props) => {
 
                     if (chatsFoundCount >= chatIds.length) {
                         dispatch(setChatsData({ chatsData }));
+                        setIsLoading(false);
                     }
                 });
+
+                if (chatsFoundCount == 0) {
+                    setIsLoading(false);
+                }
             };
 
             console.log(chatIds);
@@ -134,6 +144,11 @@ const MainNavigator = (props) => {
         }
     }, []);
 
+    if (isLoading) {
+        <View style={commonStyles.center}>
+            <ActivityIndicator size={'large'} color={colors.primary} />
+        </View>
+    }
 
     return (
         <StackNavigator />
