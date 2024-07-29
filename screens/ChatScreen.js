192d216be607fb2875db316fbdcb36b4b5cdcf81
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-07-29 23:20:12 +0300

    🚀Project added

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -24,6 +24,8 @@ import { createChat, sendImage, sendTextMessage } from "../utils/actions/chatAct
 import ReplyTo from "../components/ReplyTo";
 import { launchImagePicker, openCamera, uploadImageAsync } from "../utils/imagePickerHelper";
 import AwesomeAlert from "react-native-awesome-alerts";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const ChatScreen = (props) => {
     const [chatUsers, setChatUsers] = useState([]);
@@ -72,7 +74,22 @@ const ChatScreen = (props) => {
 
     useEffect(() => {
         props.navigation.setOptions({
-            headerTitle: title
+            headerTitle: title,
+            headerRight: () => {
+                return <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                    {
+                        chatId &&
+                        <Item
+                            title="Chat settings"
+                            iconName="settings-outline"
+                            onPress={() => chatData.isGroupChat ?
+                                props.navigation.navigate("") :
+                                props.navigation.navigate("Contact", { uid: chatUsers.find(uid => uid !== userData.userId) })
+                            }
+                        />
+                    }
+                </HeaderButtons>
+            }
         })
         setChatUsers(chatData.users)
     }, [chatUsers])
