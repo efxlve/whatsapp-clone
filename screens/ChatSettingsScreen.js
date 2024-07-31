eae7a1be12627f1f4284f02ca25d49fc262c658d
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-07-31 12:52:04 +0300

    🚀Project added

--- screens/ChatSettingsScreen.js
+++ screens/ChatSettingsScreen.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer, useState } from 'react';
+import React, { useCallback, useEffect, useReducer, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, ActivityIndicator } from 'react-native';
 import { useSelector } from 'react-redux';
 import Input from '../components/Input';
@@ -7,7 +7,7 @@ import PageTitle from '../components/PageTitle';
 import ProfileImage from '../components/ProfileImage';
 import SubmitButton from '../components/SubmitButton';
 import colors from '../constants/colors';
-import { removeUserFromChat, updateChatData } from '../utils/actions/chatActions';
+import { addUsersToChat, removeUserFromChat, updateChatData } from '../utils/actions/chatActions';
 import { validateInput } from '../utils/actions/formActions';
 import { reducer } from '../utils/reducers/formReducer';
 import DataItem from '../components/DataItem';
@@ -29,6 +29,28 @@ const ChatSettingsScreen = props => {
     }
 
     const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+    const selectedUsers = props.route.params && props.route.params.selectedUsers;
+    useEffect(() => {
+        if (!selectedUsers) {
+            return;
+        }
+
+        const selectedUserData = [];
+        selectedUsers.forEach(uid => {
+            if (uid === userData.userId) return;
+
+            if (!storedUsers[uid]) {
+                console.log("No user data found in the data store");
+                return;
+            }
+
+            selectedUserData.push(storedUsers[uid]);
+        });
+
+        addUsersToChat(userData, selectedUserData, chatData);
+
+    }, [selectedUsers]);
 
     const inputChangedHandler = useCallback((inputId, inputValue) => {
         const result = validateInput(inputId, inputValue);
@@ -106,6 +128,7 @@ const ChatSettingsScreen = props => {
                     title="Add users"
                     icon="plus"
                     type="button"
+                    onPress={() => props.navigation.navigate("NewChat", { isGroupChat: true, existingUsers: chatData.users, chatId })}
                 />
 
                 {
--- utils/actions/chatActions.js
+++ utils/actions/chatActions.js
@@ -1,6 +1,6 @@
 import { child, get, getDatabase, push, ref, remove, set, update } from "firebase/database";
 import { getFirebaseApp } from "../firebaseHelper";
-import { deleteUserChat, getUserChats } from "./userActions";
+import { addUserChat, deleteUserChat, getUserChats } from "./userActions";
 
 export const createChat = async (loggedInUserId, chatData) => {
 
@@ -125,7 +125,36 @@ export const removeUserFromChat = async (userLoggedInData, userToRemoveData, cha
 
     const messageText = userLoggedInData.userId === userToRemoveData.userId ?
         `${userLoggedInData.firstName} left the chat` :
-        `${userLoggedInData.firstName} removed ${userToRemoveData.firstName} from the chat`;
+        `${userLoggedInData.firstName} ${userLoggedInData.lastName} removed ${userToRemoveData.firstName} ${userToRemoveData.lastName} from the chat`;
 
     await sendInfoMessage(chatData.key, userLoggedInData.userId, messageText);
 };
+
+export const addUsersToChat = async (userLoggedInData, usersToAddData, chatData) => {
+    const existingUsers = Object.values(chatData.users);
+    const newUsers = [];
+
+    let userAddedName = "";
+
+    usersToAddData.forEach(async userToAdd => {
+        const userToAddId = userToAdd.userId;
+
+        if (existingUsers.includes(userToAddId)) return;
+
+        newUsers.push(userToAddId);
+
+        await addUserChat(userToAddId, chatData.key);
+
+        userAddedName = `${userToAdd.firstName} ${userToAdd.lastName}`;
+    });
+
+    if (newUsers.length === 0) {
+        return;
+    }
+
+    await updateChatData(chatData.key, userLoggedInData.userId, { users: existingUsers.concat(newUsers) })
+
+    const moreUsersMessage = newUsers.length > 1 ? `and ${newUsers.length - 1} others ` : '';
+    const messageText = `${userLoggedInData.firstName} ${userLoggedInData.lastName} added ${userAddedName} ${moreUsersMessage}to the chat`;
+    await sendInfoMessage(chatData.key, userLoggedInData.userId, messageText);
+}
